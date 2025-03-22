b604cb71d29d87c1d669ebe351956ba122deba24
Sandeep <sandeepdewangan7@gmail.com>
2025-03-22 09:42:38 +0530

    Appwrite constants setup

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const databaseId = "67de3803001dedf8b026";
+  static const String projectId = "661ba0f74b04d6a317ad";
+  static const String endPoint = "https://cloud.appwrite.io/v1";
+}
